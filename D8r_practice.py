e2622084b96f8b1b0fc7f0c4f4dbfad833cb9570
vasireddymanasadurgamanasadurg-lab <vasireddymanasadurgamanasadurg@gmail.com>
2026-01-05 19:18:46 +0530

    Add files via upload

---
+++ D8r_practice.py
@@ -0,0 +1,19 @@
+
+Ratings = float(input("Enter Movie Rating out of 5: "))
+
+if Ratings >= 4.5:
+    print("Movie is BLOCKBUSTER")
+elif Ratings >= 3.5:
+    print("Movie is Hit")
+elif Ratings >= 3:
+    print("Movie is Good")
+elif Ratings >= 2:
+    print("Movie is Average")
+elif Ratings >= 1.5:
+    print("Movie is Below Average")
+else:
+    print("Movie is Flop")
+
+    
+
+
